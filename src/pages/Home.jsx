4ee14d25218fc68e6116a2a4c15b9a6f3ac150e5
Mateus Monteiro <ceomonteiro.finral@gmail.com>
2026-07-23 12:35:13 -0300

    Adicionando botão novo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,6 +41,13 @@ function Home() {
         }
     };
 
+    const handleSwapLanguages = () => {
+        setSourceLang(targetLang);
+        setTargeLang(sourceLang);
+        setSourceText(textTranslate);
+        setTextTranslate(sourceText);
+    };
+
     useEffect(() => {
         const timer = setTimeout(() => {
             handleTranlsate()
@@ -68,7 +75,7 @@ function Home() {
                                 ))}
                             </select>
 
-                            <button className="p-2 rounded-full hover:bg-gray-100 outline-none">
+                            <button className="p-2 rounded-full hover:bg-gray-100 outline-none" onClick={handleSwapLanguages}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
                                 </svg>
@@ -93,7 +100,7 @@ function Home() {
                                     </Activity>
                                 </div>
 
-                                    <p className="text-lg text-text">{textTranslate}</p>
+                                <p className="text-lg text-text">{textTranslate}</p>
                             </div>
                         </div>
                     </div>
